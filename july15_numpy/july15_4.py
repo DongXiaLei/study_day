d3498b3267ecf237d41ae8a3046f42d5d97b3cd0
labWindows <1594003819@qq.com>
2018-07-15 16:24:21 +0800

    july15 Sun afternoon pandas:Series,Dataframe, merge index

--- july15_numpy/july15_4.py
+++ july15_numpy/july15_4.py
@@ -9,7 +9,7 @@ a = math.pi/2
 x = np.linspace(0, a, 1000)
 y = np.sqrt(np.sin(2*x) * np.sin(x))
 
-file = open(r'C:\Users\15940\Desktop\readme.txt','r')
+file = open(r'C:\Users\Administrator\Desktop\readme.txt','r')
 striing = file.readlines()
 print(striing)
 
